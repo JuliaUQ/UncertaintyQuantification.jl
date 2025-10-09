a98292e0f047af8c57e97b2cd11e051b7f7a4670
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2025-10-09 12:08:29 +0200

    Add basis function types

---
+++ src/models/basisfunctions/radialbasis.jl
@@ -0,0 +1,11 @@
+abstract type AbstractRadialBasis end
+
+struct GaussianRadialBasis <: AbstractRadialBasis
+    c::AbstractMatrix{<:Real}
+    σ::Real
+end
+
+struct PolyharmonicRadialBasis <: AbstractRadialBasis
+    c::AbstractMatrix{<:Real}
+    k::Int
+end
