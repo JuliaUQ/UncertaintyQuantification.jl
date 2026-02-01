2a33fe98a67f359a0c72a4aeaa035316a65361ec
felixmett <felixmett@googlemail.com>
2026-02-01 18:24:31 +0100

    Simplify gaussian process in- and output standardization

--- src/models/gp/standardization.jl
+++ src/models/gp/standardization.jl
@@ -1,92 +1,232 @@
-abstract type AbstractDataTransform end
+abstract type AbstractTransformChoice end
 
-# ---------------- Input/Output transforms ----------------
 """
-    IdentityTransform()
+    IdentityTransformChoice()
 
-A standardization transform that applies an identity transform to data.
+A standardization choice that specifies the application of an identity transform to data.
 
 Used as an input or output transformation in a [`GaussianProcess`](@ref). 
 Internally, the `DataStandardizer` constructs the functions required for evaluation.
 
 # Examples
 ```jldoctest
-julia> id = UncertaintyQuantification.IdentityTransform()
-IdentityTransform()
+julia> id = IdentityTransformChoice()
+IdentityTransformChoice()
 ```
 """
-struct IdentityTransform <: AbstractDataTransform end
+struct IdentityTransformChoice <: AbstractTransformChoice end
 
 """
-    ZScoreTransform()
+    ZScoreTransformChoice()
 
-A standardization transform that rescales data to zero mean and unit variance. 
+A standardization choice that specifies the application of a Z-score-transformation to data.
 
 Used as an input or output transformation in a [`GaussianProcess`](@ref). 
 Internally, the `DataStandardizer` constructs the functions required for evaluation.
 
 # Examples
 ```jldoctest
-julia> zscore = UncertaintyQuantification.ZScoreTransform()
-ZScoreTransform()
+julia> zscore = ZScoreTransformChoice()
+ZScoreTransformChoice()
 ```
 """
-struct ZScoreTransform <: AbstractDataTransform end
+struct ZScoreTransformChoice <: AbstractTransformChoice end
 
 """
-    UnitRangeTransform()
+    UnitRangeTransformChoice()
 
-A standardization transform that rescales data to the [0, 1] range.
+A standardization choice that specifies the application of a unit range transform to data.
 
 Used as an input or output transformation in a [`GaussianProcess`](@ref). 
 Internally, the `DataStandardizer` constructs the functions required for evaluation.
 
 # Examples
 ```jldoctest
-julia> unitrange = UncertaintyQuantification.UnitRangeTransform()
-UnitRangeTransform()
+julia> unitrange = UnitRangeTransformChoice()()
+UnitRangeTransformChoice()()
 ```
 """
-struct UnitRangeTransform <: AbstractDataTransform end
+struct UnitRangeTransformChoice <: AbstractTransformChoice end
 
 """
-    StandardNormalTransform()
+    StandardNormalTransformChoice()
 
-A normalization transform that transforms data to the standard normal space. 
+A standardization choice that specifies the application of a standard normal transformation to data. 
 
 Can only be used as an input transformation in a [`GaussianProcess`](@ref) for inputs of type [`RandomVariable`](@ref). 
 Internally, the `DataStandardizer` constructs the function required for evaluation.
 
 # Examples
 ```jldoctest
-julia> sns = StandardNormalTransform()
-StandardNormalTransform()
+julia> sns = StandardNormalTransformChoice()
+StandardNormalTransformChoice()
 ```
 """
-struct StandardNormalTransform <: AbstractDataTransform end
+struct StandardNormalTransformChoice <: AbstractTransformChoice end
 
-struct InputTransform{T <: AbstractDataTransform} end
-InputTransform(::Type{T}) where {T <: AbstractDataTransform} = InputTransform{T}()
-InputTransform(x::AbstractDataTransform) = InputTransform(typeof(x))
 
-struct OutputTransform{T <: AbstractDataTransform} end
-OutputTransform(::Type{T}) where {T <: AbstractDataTransform} = OutputTransform{T}()
-OutputTransform(x::AbstractDataTransform) = OutputTransform(typeof(x))
+# ---------------- Utility ----------------
+to_gp_format(x::Vector) = x
+to_gp_format(x::Matrix) = RowVecs(x)
+dataframe_to_array(df::DataFrame, name::Symbol) = df[:, name]
+dataframe_to_array(df::DataFrame, names::Vector{<:Symbol}) = length(names) == 1 ? x = dataframe_to_array(df, only(names)) : x = Matrix(df[:, names])
 
-# ---
+# Internal transform types for dispatching
+struct NoTransform end
+struct StandardNormalTransform end
+
+
+# ---------------- Input transformation ----------------
 # # Developer Note
+# Gaussian process regression inputs are always transformed to Vector or RowVecs in the multivariate input case
+struct GaussianProcessInputTransform{T}
+    transform::T
+    input::Union{Symbol, Vector{<:Symbol}, UQInput, Vector{<:UQInput}}
+end
 
-# DataStandardizer bundles input and output transformation functions for Gaussian process models.
+# Fitting
+fit_input_transform(
+    ::DataFrame, 
+    input::Union{Symbol, Vector{<:Symbol}}, 
+    ::IdentityTransformChoice
+) = GaussianProcessInputTransform(NoTransform(), input)
 
-# # Fields
+function fit_input_transform(
+    data::DataFrame, 
+    input::Union{Symbol, Vector{<:Symbol}}, 
+    ::ZScoreTransformChoice
+) 
+    transform = fit(
+        StatsBase.ZScoreTransform, 
+        dataframe_to_array(data, input); 
+        dims=1
+    )
+    return GaussianProcessInputTransform(transform, input)
+end
 
-# - `fᵢ` - function applied to input data.
-# - `fₒ` - function applied to output data.
-# - `fₒ⁻¹` - inverse function for the output transformation.
-# - `var_fₒ⁻¹` - function for transforming output variances.
+function fit_input_transform(
+    data::DataFrame, 
+    input::Union{Symbol, Vector{<:Symbol}}, 
+    ::UnitRangeTransformChoice
+) 
+    transform = fit(
+        StatsBase.UnitRangeTransform, 
+        dataframe_to_array(data, input); 
+        dims=1
+    )
+    return GaussianProcessInputTransform(transform, input)
+end
 
-# !!! note "Inverse output transformations"
+fit_input_transform(
+    ::DataFrame, 
+    input::Union{Symbol, Vector{<:Symbol}}, 
+    ::StandardNormalTransformChoice
+) = throw(ArgumentError("Standard normal input transform is only valid for inputs of type UQInput"))
 
+fit_input_transform(
+    data::DataFrame, 
+    input::Union{UQInput, Vector{<:UQInput}}, 
+    choice::Union{IdentityTransformChoice, ZScoreTransformChoice, UnitRangeTransformChoice}
+) = fit_input_transform(data, names(input), choice)
+
+fit_input_transform(
+    ::DataFrame, 
+    input::Union{UQInput, Vector{<:UQInput}}, 
+    ::StandardNormalTransformChoice
+) = GaussianProcessInputTransform(StandardNormalTransform(), input)
+
+# Transforms
+transform(
+    data::DataFrame, 
+    transformer::GaussianProcessInputTransform{NoTransform}
+) = to_gp_format(dataframe_to_array(data, transformer.input))
+
+transform(
+    data::DataFrame,
+    transformer::Union{GaussianProcessInputTransform{<:ZScoreTransform}, GaussianProcessInputTransform{<:UnitRangeTransform}}
+) = to_gp_format(
+    StatsBase.transform(
+            transformer.transform,
+            dataframe_to_array(data, transformer.input)
+        )
+    )
+
+function transform(
+    data::DataFrame, 
+    transformer::GaussianProcessInputTransform{StandardNormalTransform}
+)
+    data_copy = copy(data)
+    to_standard_normal_space!(transformer.input, data_copy)
+    return to_gp_format(dataframe_to_array(data_copy, names(transformer.input)))
+end 
+
+
+# ---------------- Output transformation ----------------
+# # Developer Note
+# Gaussian process regression target outputs are always extracted from DataFrame and transformed to Vector
+# Inverse transforms return Vectors that later get inserted in the provided DataFrame that is used in evaluate! method for GaussianProcess
+struct GaussianProcessOutputTransform{T}
+    transform::T
+    output::Symbol
+end
+
+# Fitting
+fit_output_transform(
+    ::DataFrame, 
+    output::Symbol, 
+    ::IdentityTransformChoice
+) = GaussianProcessOutputTransform(NoTransform(), output)
+
+function fit_output_transform(
+    data::DataFrame, 
+    output::Symbol, 
+    ::ZScoreTransformChoice
+) 
+    transform = fit(
+        StatsBase.ZScoreTransform, 
+        dataframe_to_array(data, output); 
+        dims=1
+    )
+    return GaussianProcessOutputTransform(transform, output)
+end
+
+function fit_output_transform(
+    data::DataFrame, 
+    output::Symbol, 
+    ::UnitRangeTransformChoice
+) 
+    transform = fit(
+        StatsBase.UnitRangeTransform, 
+        dataframe_to_array(data, output); 
+        dims=1
+    )
+    return GaussianProcessOutputTransform(transform, output)
+end
+
+fit_output_transform(
+    ::DataFrame, 
+    ::Symbol, 
+    ::StandardNormalTransformChoice
+) = throw(ArgumentError("Standard normal transform for outputs is not possible"))
+
+# Transforms
+transform(
+    data::DataFrame, 
+    transformer::GaussianProcessOutputTransform{NoTransform}
+) = to_gp_format(dataframe_to_array(data, transformer.output))
+
+transform(
+    data::DataFrame,
+    transformer::Union{GaussianProcessOutputTransform{<:ZScoreTransform}, GaussianProcessOutputTransform{<:UnitRangeTransform}}
+) = to_gp_format(
+    StatsBase.transform(
+            transformer.transform,
+            dataframe_to_array(data, transformer.output)
+        )
+    )
+
+# Inverse transforms
+# # Developer Note
 # Gaussian process regression requires two distinct inverse transformations for the output:
 # one for the mean predictions (this same transformation can also be applied to function samples) and one for the variance predictions.
 
@@ -106,213 +246,22 @@ OutputTransform(x::AbstractDataTransform) = OutputTransform(typeof(x))
 #     ```math
 #     Var[y] = E[(σ\tilde{y} + μ - E[σ\tilde{y} + μ])^2] = E[(σ^2(\tilde{y} - E[\tilde{y}])^2] = σ^2 Var[\tilde{y}]
 #     ```
-# Hence, `fₒ⁻¹` and `var_fₒ⁻¹` must be implemented separately.
+inverse_transform(
+    data::AbstractArray, 
+    ::GaussianProcessOutputTransform{NoTransform}
+) = data
 
-# # Constructor
+inverse_transform(
+    data::AbstractArray,
+    transformer::Union{GaussianProcessOutputTransform{<:ZScoreTransform}, GaussianProcessOutputTransform{<:UnitRangeTransform}}
+) = StatsBase.reconstruct(transformer.transform, data)
 
-#     DataStandardizer(
-#         data::DataFrame, 
-#         input::Union{Symbol, Vector{<:Symbol}, UQInput, Vector{<:UQInput}}, 
-#         output::Symbol, 
-#         input_transform::InputTransform, 
-#         output_transform::OutputTransform
-#     )
+variance_inverse_transform(
+    data::AbstractArray,
+    ::GaussianProcessOutputTransform{NoTransform}
+) = data
 
-# Constructs a set of transformation functions from the provided data and user-specified input/output transforms. 
-# Internally, it uses `build_datatransform` to create the actual functions.
-
-# # Purpose
-
-# This struct allows [`GaussianProcess`](@ref) models to consistently apply input and output transformations 
-#     (like `ZScoreTransform` or `IdentityTransform`) while keeping the API simple for end-users. 
-# The `AbstractDataTransform` structs signal the desired behavior, and `DataStandardizer` converts them into callable functions for internal use.
-# ---
-struct DataStandardizer
-    fᵢ::Function
-    fₒ::Function
-    fₒ⁻¹::Function
-    var_fₒ⁻¹::Function
-end
-
-function DataStandardizer(
-    data::DataFrame,
-    input::Union{Symbol, Vector{<:Symbol}, UQInput, Vector{<:UQInput}},
-    output::Symbol,
-    input_transform::InputTransform, 
-    output_transform::OutputTransform
-)
-    fᵢ = build_datatransform(data, input, input_transform)
-    fₒ, fₒ⁻¹, var_fₒ⁻¹ = build_datatransform(data, output, output_transform)
-    return DataStandardizer(fᵢ, fₒ, fₒ⁻¹, var_fₒ⁻¹)
-end
-
-
-# ---
-# build_datatransform(data, input/output, transform)
-#
-# Returns a function (or pair of functions for outputs) that applies the specified 
-# transformation to a dataframe.
-# ---
-
-# ---------------- Input ----------------
-# No input transformation
-function build_datatransform(
-    ::DataFrame, 
-    input::Union{Symbol, Vector{<:Symbol}}, 
-    ::InputTransform{IdentityTransform}
-)
-    f(df::DataFrame) = to_gp_format(
-        dataframe_to_array(df, input)
-    )
-    return f
-end
-
-build_datatransform(
-    data::DataFrame, 
-    input::Union{UQInput, Vector{<:UQInput}}, 
-    transform::InputTransform{IdentityTransform}
- ) = build_datatransform(data, names(input), transform)
-
- # ZScore input transformation
-function build_datatransform(
-    data::DataFrame,
-    input::Union{Symbol, Vector{<:Symbol}},
-    ::InputTransform{ZScoreTransform}
-)
-    zscore_transform = fit(
-        StatsBase.ZScoreTransform, 
-        dataframe_to_array(data, input); 
-        dims=1
-    )
-    f(df::DataFrame) = to_gp_format(
-        StatsBase.transform(
-            zscore_transform, 
-            dataframe_to_array(df, input)
-        )
-    )
-    return f
-end
-
-build_datatransform(
-    data::DataFrame, 
-    input::Union{UQInput, Vector{<:UQInput}}, 
-    transform::InputTransform{ZScoreTransform}
- ) = build_datatransform(data, names(input), transform)
-
-# UnitRange input transformation
-function build_datatransform(
-    data::DataFrame,
-    input::Union{Symbol, Vector{<:Symbol}},
-    ::InputTransform{UnitRangeTransform}
-)
-    unitrange_transform = fit(
-        StatsBase.UnitRangeTransform, 
-        dataframe_to_array(data, input); 
-        dims=1
-    )
-    f(df::DataFrame) = to_gp_format(
-        StatsBase.transform(
-            unitrange_transform, 
-            dataframe_to_array(df, input)
-        )
-    )
-    return f
-end
-
-build_datatransform(
-    data::DataFrame, 
-    input::Union{UQInput, Vector{<:UQInput}}, 
-    transform::InputTransform{UnitRangeTransform}
- ) = build_datatransform(data, names(input), transform)
-
-# SNS input transform
-function build_datatransform(
-    ::DataFrame,
-    input::Union{UQInput, Vector{<:UQInput}},
-    ::InputTransform{StandardNormalTransform}
-)
-    function f(df::DataFrame)
-        df_copy = copy(df)
-        to_standard_normal_space!(input, df_copy)
-        return to_gp_format(
-            dataframe_to_array(df_copy, names(input))    
-        )
-    end
-    return f
-end
-
-# ---------------- Output ----------------
-# No output transformation
-function build_datatransform(
-    ::DataFrame, 
-    output::Symbol, 
-    ::OutputTransform{IdentityTransform}
-)
-    f(df::DataFrame) = to_gp_format(
-        dataframe_to_array(df, output)
-    )
-    f⁻¹(Y::AbstractArray) = Y
-    var_f⁻¹(Y::AbstractArray) = Y
-    return (f, f⁻¹, var_f⁻¹)
-end
-
-# ZScore output transformation
-function build_datatransform(
-    data::DataFrame, 
-    output::Symbol,
-    ::OutputTransform{ZScoreTransform}
-)
-    zscore_transform = fit(
-        StatsBase.ZScoreTransform, 
-        dataframe_to_array(data, output); 
-        dims=1
-    )
-    f(df::DataFrame) = to_gp_format(
-        StatsBase.transform(
-            zscore_transform, 
-            dataframe_to_array(df, output)
-        )
-    )
-    f⁻¹(Y::AbstractArray) = StatsBase.reconstruct(zscore_transform, Y)
-    var_f⁻¹(Y::AbstractArray) = only(zscore_transform.scale)^2 * Y 
-    return (f, f⁻¹, var_f⁻¹)
-end
-
-# UnitRange output transformation
-function build_datatransform(
-    data::DataFrame, 
-    output::Symbol,
-    ::OutputTransform{UnitRangeTransform}
-)
-    unitrange_transform = fit(
-        StatsBase.UnitRangeTransform, 
-        dataframe_to_array(data, output); 
-        dims=1
-    )
-    f(df::DataFrame) = to_gp_format(
-        StatsBase.transform(
-            unitrange_transform, 
-            dataframe_to_array(df, output)
-        )
-    )
-    f⁻¹(Y::AbstractArray) = StatsBase.reconstruct(unitrange_transform, Y)
-    var_f⁻¹(Y::AbstractArray) = only(unitrange_transform.scale)^2 * Y 
-    return (f, f⁻¹, var_f⁻¹)
-end
-
-function build_datatransform(
-    ::DataFrame, 
-    ::Symbol, 
-    ::OutputTransform{StandardNormalTransform}
-)
-    throw(ArgumentError(
-        "StandardNormalTransform is only valid for input transforms."
-    ))
-end
-
-# ---------------- Utility ----------------
-to_gp_format(x::Vector) = x
-to_gp_format(x::Matrix) = RowVecs(x)
-
-dataframe_to_array(df::DataFrame, name::Symbol) = df[:, name]
-dataframe_to_array(df::DataFrame, names::Vector{<:Symbol}) = length(names) == 1 ? x = dataframe_to_array(df, only(names)) : x = Matrix(df[:, names])
+variance_inverse_transform(
+    data::AbstractArray,
+    transformer::Union{GaussianProcessOutputTransform{<:ZScoreTransform}, GaussianProcessOutputTransform{<:UnitRangeTransform}}
+) = only(transformer.transform.scale)^2 * data
