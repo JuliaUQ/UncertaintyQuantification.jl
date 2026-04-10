6d3b59304aec7edcc09acb160a306924bfc826c7
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2026-04-10 13:00:08 +0200

    Remove old evaluate method

--- src/models/responsesurface.jl
+++ src/models/responsesurface.jl
@@ -26,31 +26,3 @@ function ResponseSurface(
     basis = MonomialBasis(length(inputs), p; include_zero=true)
     return LinearBasisFunctionModel(data, output, basis, inputs)
 end
-
-"""
-    evaluate!(rs::ResponseSurface, data::DataFrame)
-
-evaluating data by using a previously trained ResponseSurface.
-
-
-# Examples
-
-```jldoctest
-julia> data = DataFrame(x = 1:10, y = [1, 4, 10, 15, 24, 37, 50, 62, 80, 101]);
-
-julia> rs = ResponseSurface(data, :y, 2);
-
-julia> df = DataFrame(x = [2.5, 11, 15]);
-
-julia> evaluate!(rs, df);
-
-julia> df.y |> DisplayAs.withcontext(:compact => true)
-3-element Vector{Float64}:
-   6.25511
- 121.15
- 226.165
-```
-"""
-function evaluate!(rs::ResponseSurface, data::DataFrame)
-    return evaluate!(rs.m, data)
-end
