abd1cb52ebe229534eaa901dfbe92709bb1a0c1e
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2026-04-10 12:31:57 +0200

    Make ResponseSurface an alias

--- src/models/responsesurface.jl
+++ src/models/responsesurface.jl
@@ -11,24 +11,20 @@ julia> rs = ResponseSurface(data, :y, 2)
 ResponseSurface([0.48333333333332457, -0.23863636363636026, 1.0189393939393936], :y, [:x], 2, Monomials.Monomial[1, x1, x1²])
 ```
 """
-struct ResponseSurface <: UQModel
-    m::LinearBasisFunctionModel
+const ResponseSurface = LinearBasisFunctionModel{MonomialBasis}
 
-    function ResponseSurface(
-        data::DataFrame,
-        output::Symbol,
-        p::Int,
-        inputs::Vector{Symbol}=propertynames(data[:, Not(output)]),
-    )
-        if p < 0
-            error("Degree(p) of ResponseSurface must be non-negative.")
-        end
-
-        basis = MonomialBasis(length(inputs), p; include_zero=true)
-        m = LinearBasisFunctionModel(data, output, basis, inputs)
-
-        return new(m)
+function ResponseSurface(
+    data::DataFrame,
+    output::Symbol,
+    p::Int,
+    inputs::Vector{Symbol}=propertynames(data[:, Not(output)]),
+)
+    if p < 0
+        error("Degree(p) of ResponseSurface must be non-negative.")
     end
+
+    basis = MonomialBasis(length(inputs), p; include_zero=true)
+    return LinearBasisFunctionModel(data, output, basis, inputs)
 end
 
 """
