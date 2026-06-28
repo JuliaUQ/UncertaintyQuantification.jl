9c25a07e309f9a05b8e639622d89e02e72120b4a
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2026-06-28 09:04:08 +0200

    Don't run random slicing in parallel

--- src/models/imprecise/propagation.jl
+++ src/models/imprecise/propagation.jl
@@ -29,7 +29,7 @@ function propagate_intervals!(
 
     output = name(models[end])
 
-    y = pmap(eachrow(df)) do row
+    y = map(eachrow(df)) do row
         degenerates = isdegenerate.(collect(row[interval_names]))
         pure = .!degenerates
 
